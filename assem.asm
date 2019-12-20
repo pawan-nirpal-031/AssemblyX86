acc9c30a0f04a6cc7a60b22472477c2d9fa1a6cf
Pawan Nirpal <49726539+pawan-nirpal-031@users.noreply.github.com>
2019-12-20 17:30:29 +0530

    Update assem.asm

--- assem.asm
+++ assem.asm
@@ -1,5 +1,10 @@
+section .bss
+buf resb 15
+buf_len  equ $-buf
+
+
 section .data
-msg db 10,"Hello Microprocesser ",10
+msg db 10,"input name ",10
 msg_len equ $-msg
 
 msg1 db 10,"New program",10
@@ -8,7 +13,7 @@ msg1_len equ $-msg1
 msg2 db 10,"Assmebly program 2",10
 msg2_len equ $-msg2
 
-%macro cout 2
+	%macro cout 2
 	mov rax,1
 	mov rdi,1
 	mov rsi,%1
@@ -21,6 +26,14 @@ msg2_len equ $-msg2
 	mov rdi,00
 	syscall
 	%endmacro
+    
+	%macro cin 2
+	mov rax,00
+	mov rdi,00
+	mov rsi,%1
+	mov rdx,%2
+	syscall
+	%endmacro
 
 section .text
 	global _start
@@ -29,8 +42,10 @@ _start:
 
 	cout msg,msg_len
 
-	cout msg1,msg1_len
+    cin buf,buf_len
 
-	cout msg2,msg2_len
-	 
+	cout msg1,msg1_len
+    
+	cout buf,buf_len
+	
 	exit
